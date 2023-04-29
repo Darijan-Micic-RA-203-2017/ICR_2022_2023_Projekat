82d8beb869eb797f5501d9455449b4746b3dfedf
Darijan Mićić <darijan.micic10@gmail.com>
2023-04-29 02:11:00 +0200

    feat: create key bindings for menu items
    Reference: https://codesamplez.com/development/wpf-hotkeys-c-sharp

--- ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/MainWindow.xaml.cs
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/MainWindow.xaml.cs
@@ -22,6 +22,96 @@ namespace ICR_2022_2023_Mapa_dogadjaja
         public MainWindow()
         {
             InitializeComponent();
+
+            AddHotKeys();
+        }
+
+        // REFERENCE: https://codesamplez.com/development/wpf-hotkeys-c-sharp
+        private void AddHotKeys()
+        {
+            try
+            {
+                RoutedCommand closeApplicationCommand = new RoutedCommand();
+                closeApplicationCommand.InputGestures.Add(new KeyGesture(Key.Q, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(closeApplicationCommand, CloseApplication));
+
+                RoutedCommand openDialogForCreatingANewEventCommand = new RoutedCommand();
+                openDialogForCreatingANewEventCommand.InputGestures.Add(new KeyGesture(Key.N, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForCreatingANewEventCommand, OpenDialogForCreatingANewEvent));
+
+                RoutedCommand openDialogForEditingAnEventCommand = new RoutedCommand();
+                openDialogForEditingAnEventCommand.InputGestures.Add(new KeyGesture(Key.E, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForEditingAnEventCommand, OpenDialogForEditingAnEvent));
+
+                RoutedCommand openDialogForDeletingAnEntityCommand = new RoutedCommand();
+                openDialogForDeletingAnEntityCommand.InputGestures.Add(new KeyGesture(Key.Delete, ModifierKeys.None));
+                CommandBindings.Add(new CommandBinding(openDialogForDeletingAnEntityCommand, OpenDialogForDeletingAnEntity));
+                
+                RoutedCommand openDialogForAllEventTagsCommand = new RoutedCommand();
+                openDialogForAllEventTagsCommand.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllEventTagsCommand, OpenDialogForAllEventTags));
+
+                RoutedCommand openDialogForAllEventTypesCommand = new RoutedCommand();
+                openDialogForAllEventTypesCommand.InputGestures.Add(new KeyGesture(Key.T, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllEventTypesCommand, OpenDialogForAllEventTypes));
+
+                RoutedCommand openDialogForAllPopulatedPlacesCommand = new RoutedCommand();
+                openDialogForAllPopulatedPlacesCommand.InputGestures.Add(new KeyGesture(Key.G, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllPopulatedPlacesCommand, OpenDialogForAllPopulatedPlaces));
+
+                RoutedCommand openDialogForAllCountriesCommand = new RoutedCommand();
+                openDialogForAllCountriesCommand.InputGestures.Add(new KeyGesture(Key.R, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllCountriesCommand, OpenDialogForAllCountries));
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.StackTrace);
+            }
+        }
+
+        private void CloseApplication(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OpenDialogForCreatingANewEvent(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Create a new event Ctrl + N");
+        }
+
+        private void OpenDialogForEditingAnEvent(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Edit an event Ctrl + E");
+        }
+
+        private void OpenDialogForDeletingAnEntity(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Delete an entity Delete");
+        }
+
+        private void OpenDialogForAllEventTags(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("All event tags Ctrl + I");
+        }
+
+        private void OpenDialogForAllEventTypes(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("All event types Ctrl + T");
+        }
+
+        private void OpenDialogForAllPopulatedPlaces(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("All populated places Ctrl + G");
+        }
+
+        private void OpenDialogForAllCountries(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("All countries Ctrl + R");
+        }
+
+        private void OpenHelpDialog(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Help Alt + P");
         }
     }
 }
