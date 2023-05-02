ab3b96652da5a1c0fa6c3dfa8ca29ad1b5776ea1
Darijan Mićić <darijan.micic10@gmail.com>
2023-05-02 15:13:54 +0200

    improvement: add converter for date in table

---
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/Converter/DateTimeToFormattedStringConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ICR_2022_2023_Mapa_dogadjaja.Converter
+{
+    public class DateTimeToFormattedStringConverter : MarkupExtension, IValueConverter
+    {
+        public DateTimeToFormattedStringConverter() { }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime) value;
+
+            string dateTimeAsString;
+            try
+            {
+                dateTimeAsString = dateTime.ToString("MM/dd/yyyy");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+
+                return null;
+            }
+
+            return dateTimeAsString;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string dateTimeAsString = value as string;
+
+            DateTime dateTime;
+            if (DateTime.TryParse(dateTimeAsString, out dateTime))
+            {
+                return dateTime;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
