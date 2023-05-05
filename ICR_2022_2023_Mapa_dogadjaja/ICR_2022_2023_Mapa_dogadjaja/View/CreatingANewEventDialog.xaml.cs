d6c67810c2ca4a96ae8099fe7506d40a361b684c
Darijan Mićić <darijan.micic10@gmail.com>
2023-05-05 20:29:45 +0200

    feat: create dialog for creating a new event

---
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreatingANewEventDialog.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ICR_2022_2023_Mapa_dogadjaja.View
+{
+    /// <summary>
+    /// Interaction logic for CreatingANewEventDialog.xaml
+    /// </summary>
+    public partial class CreatingANewEventDialog : Window
+    {
+        private MainWindow mainVindow;
+
+        public CreatingANewEventDialog(MainWindow mainVindow)
+        {
+            InitializeComponent();
+
+            this.mainVindow = mainVindow;
+
+            DataContext = this.mainVindow;
+
+            AddHotKeys();
+        }
+
+        public MainWindow MainWindow
+        {
+            get { return mainVindow; }
+            set { mainVindow = value; }
+        }
+
+        // REFERENCE: https://codesamplez.com/development/wpf-hotkeys-c-sharp
+        private void AddHotKeys()
+        {
+            try
+            {
+                RoutedCommand closeDialogCommand = new RoutedCommand();
+                closeDialogCommand.InputGestures.Add(new KeyGesture(Key.Q, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(closeDialogCommand, CloseDialog));
+
+                /*
+                RoutedCommand openDialogForCreatingANewEventCommand = new RoutedCommand();
+                openDialogForCreatingANewEventCommand.InputGestures.Add(new KeyGesture(Key.N, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForCreatingANewEventCommand, OpenDialogForCreatingANewEvent));
+
+                RoutedCommand openDialogForEditingAnEventCommand = new RoutedCommand();
+                openDialogForEditingAnEventCommand.InputGestures.Add(new KeyGesture(Key.E, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForEditingAnEventCommand, OpenDialogForEditingAnEvent));
+
+                RoutedCommand openDialogForDeletingAnEntityCommand = new RoutedCommand();
+                openDialogForDeletingAnEntityCommand.InputGestures.Add(new KeyGesture(Key.Delete, ModifierKeys.None));
+                CommandBindings.Add(new CommandBinding(openDialogForDeletingAnEntityCommand, OpenDialogForDeletingAnEntity));
+
+                RoutedCommand openDialogForAllEventTagsCommand = new RoutedCommand();
+                openDialogForAllEventTagsCommand.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllEventTagsCommand, OpenDialogForAllEventTags));
+
+                RoutedCommand openDialogForAllEventTypesCommand = new RoutedCommand();
+                openDialogForAllEventTypesCommand.InputGestures.Add(new KeyGesture(Key.T, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllEventTypesCommand, OpenDialogForAllEventTypes));
+
+                RoutedCommand openDialogForAllPopulatedPlacesCommand = new RoutedCommand();
+                openDialogForAllPopulatedPlacesCommand.InputGestures.Add(new KeyGesture(Key.G, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllPopulatedPlacesCommand, OpenDialogForAllPopulatedPlaces));
+
+                RoutedCommand openDialogForAllCountriesCommand = new RoutedCommand();
+                openDialogForAllCountriesCommand.InputGestures.Add(new KeyGesture(Key.R, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForAllCountriesCommand, OpenDialogForAllCountries));
+
+                RoutedCommand focusOnInputForFilteringTableCommand = new RoutedCommand();
+                focusOnInputForFilteringTableCommand.InputGestures.Add(new KeyGesture(Key.D2, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(focusOnInputForFilteringTableCommand, FocusOnInputForFilteringTable));
+
+                RoutedCommand cancelSearchOrFilteringCommand = new RoutedCommand();
+                cancelSearchOrFilteringCommand.InputGestures.Add(new KeyGesture(Key.D4, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(cancelSearchOrFilteringCommand, CancelSearchOrFiltering));
+                */
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.StackTrace);
+            }
+        }
+
+        private void CloseDialog(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
