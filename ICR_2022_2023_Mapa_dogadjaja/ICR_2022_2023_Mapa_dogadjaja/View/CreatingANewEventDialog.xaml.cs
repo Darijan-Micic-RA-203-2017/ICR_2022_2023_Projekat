3fd6444f06520396a9684d9109d07547aa6b2940
Darijan Mićić <darijan.micic10@gmail.com>
2023-05-07 17:52:58 +0200

    style: place 2 inputs per row in new event dialog
    I made this modification due to the request from assistant teacher.

--- ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreatingANewEventDialog.xaml.cs
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreatingANewEventDialog.xaml.cs
@@ -45,44 +45,6 @@ namespace ICR_2022_2023_Mapa_dogadjaja.View
                 RoutedCommand closeDialogCommand = new RoutedCommand();
                 closeDialogCommand.InputGestures.Add(new KeyGesture(Key.Q, ModifierKeys.Control));
                 CommandBindings.Add(new CommandBinding(closeDialogCommand, CloseDialog));
-
-                /*
-                RoutedCommand openDialogForCreatingANewEventCommand = new RoutedCommand();
-                openDialogForCreatingANewEventCommand.InputGestures.Add(new KeyGesture(Key.N, ModifierKeys.Control));
-                CommandBindings.Add(new CommandBinding(openDialogForCreatingANewEventCommand, OpenDialogForCreatingANewEvent));
-
-                RoutedCommand openDialogForEditingAnEventCommand = new RoutedCommand();
-                openDialogForEditingAnEventCommand.InputGestures.Add(new KeyGesture(Key.E, ModifierKeys.Control));
-                CommandBindings.Add(new CommandBinding(openDialogForEditingAnEventCommand, OpenDialogForEditingAnEvent));
-
-                RoutedCommand openDialogForDeletingAnEntityCommand = new RoutedCommand();
-                openDialogForDeletingAnEntityCommand.InputGestures.Add(new KeyGesture(Key.Delete, ModifierKeys.None));
-                CommandBindings.Add(new CommandBinding(openDialogForDeletingAnEntityCommand, OpenDialogForDeletingAnEntity));
-
-                RoutedCommand openDialogForAllEventTagsCommand = new RoutedCommand();
-                openDialogForAllEventTagsCommand.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Control));
-                CommandBindings.Add(new CommandBinding(openDialogForAllEventTagsCommand, OpenDialogForAllEventTags));
-
-                RoutedCommand openDialogForAllEventTypesCommand = new RoutedCommand();
-                openDialogForAllEventTypesCommand.InputGestures.Add(new KeyGesture(Key.T, ModifierKeys.Control));
-                CommandBindings.Add(new CommandBinding(openDialogForAllEventTypesCommand, OpenDialogForAllEventTypes));
-
-                RoutedCommand openDialogForAllPopulatedPlacesCommand = new RoutedCommand();
-                openDialogForAllPopulatedPlacesCommand.InputGestures.Add(new KeyGesture(Key.G, ModifierKeys.Control));
-                CommandBindings.Add(new CommandBinding(openDialogForAllPopulatedPlacesCommand, OpenDialogForAllPopulatedPlaces));
-
-                RoutedCommand openDialogForAllCountriesCommand = new RoutedCommand();
-                openDialogForAllCountriesCommand.InputGestures.Add(new KeyGesture(Key.R, ModifierKeys.Control));
-                CommandBindings.Add(new CommandBinding(openDialogForAllCountriesCommand, OpenDialogForAllCountries));
-
-                RoutedCommand focusOnInputForFilteringTableCommand = new RoutedCommand();
-                focusOnInputForFilteringTableCommand.InputGestures.Add(new KeyGesture(Key.D2, ModifierKeys.Alt));
-                CommandBindings.Add(new CommandBinding(focusOnInputForFilteringTableCommand, FocusOnInputForFilteringTable));
-
-                RoutedCommand cancelSearchOrFilteringCommand = new RoutedCommand();
-                cancelSearchOrFilteringCommand.InputGestures.Add(new KeyGesture(Key.D4, ModifierKeys.Alt));
-                CommandBindings.Add(new CommandBinding(cancelSearchOrFilteringCommand, CancelSearchOrFiltering));
-                */
             }
             catch (Exception e)
             {
