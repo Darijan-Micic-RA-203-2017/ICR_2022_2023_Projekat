947c5c635eefc5496e856c98d5619086bd16339d
Darijan Mićić <darijan.micic10@gmail.com>
2023-05-14 18:03:27 +0200

    feat: add icon for event by open file dialog
    This functionality is not working as intended! I have to find a reason for it.

--- ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreatingANewEventDialog.xaml.cs
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreatingANewEventDialog.xaml.cs
@@ -1,6 +1,9 @@
-﻿using ICR_2022_2023_Mapa_dogadjaja.ViewModel;
+﻿using ICR_2022_2023_Mapa_dogadjaja.Model;
+using ICR_2022_2023_Mapa_dogadjaja.ViewModel;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -21,14 +24,24 @@ namespace ICR_2022_2023_Mapa_dogadjaja.View
     {
         private AllEntitiesViewModel allEntitiesViewModel;
 
+        private string pathToSelectedEventIcon;
+
         public CreatingANewEventDialog()
         {
             InitializeComponent();
-
+            
             allEntitiesViewModel = new AllEntitiesViewModel();
             DataContext = allEntitiesViewModel;
+
+            pathToSelectedEventIcon = "";
             
             AddHotKeys();
+        }
+        
+        public string PathToSelectedEventIcon
+        {
+            get { return pathToSelectedEventIcon; }
+            set { pathToSelectedEventIcon = value; }
         }
         
         // REFERENCE: https://codesamplez.com/development/wpf-hotkeys-c-sharp
@@ -36,6 +49,10 @@ namespace ICR_2022_2023_Mapa_dogadjaja.View
         {
             try
             {
+                RoutedCommand openDialogForSelectingEventIconCommand = new RoutedCommand();
+                openDialogForSelectingEventIconCommand.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(openDialogForSelectingEventIconCommand, OpenDialogForSelectingEventIcon));
+
                 RoutedCommand saveEventCommand = new RoutedCommand();
                 saveEventCommand.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
                 CommandBindings.Add(new CommandBinding(saveEventCommand, SaveEvent));
@@ -48,6 +65,38 @@ namespace ICR_2022_2023_Mapa_dogadjaja.View
             {
                 Console.Write(e.StackTrace);
             }
+        }
+
+        // REFERENCE: https://learn.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-common-system-dialog-box?view=netdesktop-7.0
+        private void OpenDialogForSelectingEventIcon(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.gif, *.jpg, *.jpe, *.png, *.bmp, *.dib, *.tif, *.wmf, *.ras, *.eps, " + 
+                "*.pcx, *.pcd, *.tga, *.dds)|*.gif;*.jpg;*.jpe;*.png;*.bmp;*.dib;*.tif;*.wmf;*.ras;*.eps;*.pcx;*.pcd;*.tga;" + 
+                "*.dds|All files (*.*)|*.*";
+            openFileDialog.Title = "Odaberite ikonu događaja";
+            openFileDialog.AddExtension = true;
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.DereferenceLinks = true;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+            openFileDialog.ShowReadOnly = false;
+            openFileDialog.ValidateNames = true;
+
+            bool? dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult != true)
+            {
+                return;
+            }
+            
+            if (!File.Exists(openFileDialog.FileName))
+            {
+                return;
+            }
+            
+            pathToSelectedEventIcon = openFileDialog.FileName;
+            allEntitiesViewModel.PathToSelectedEventIcon = pathToSelectedEventIcon;
         }
 
         private void SaveEvent(object sender, RoutedEventArgs e)
