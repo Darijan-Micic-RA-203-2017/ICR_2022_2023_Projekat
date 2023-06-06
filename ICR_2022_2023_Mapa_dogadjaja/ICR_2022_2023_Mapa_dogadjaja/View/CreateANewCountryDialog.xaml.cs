fa3598b1df6cb8abbc97a05b2dcd593b84ed50e9
Darijan Mićić <darijan.micic10@gmail.com>
2023-06-06 20:08:22 +0200

    style: remove unnecessary imports

--- ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreateANewCountryDialog.xaml.cs
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreateANewCountryDialog.xaml.cs
@@ -7,12 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ICR_2022_2023_Mapa_dogadjaja.View
 {
--- ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreateANewEventTagDialog.xaml.cs
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/CreateANewEventTagDialog.xaml.cs
@@ -1,20 +1,14 @@
 ﻿using ICR_2022_2023_Mapa_dogadjaja.Model;
 using ICR_2022_2023_Mapa_dogadjaja.ViewModel;
-using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ICR_2022_2023_Mapa_dogadjaja.View
 {
