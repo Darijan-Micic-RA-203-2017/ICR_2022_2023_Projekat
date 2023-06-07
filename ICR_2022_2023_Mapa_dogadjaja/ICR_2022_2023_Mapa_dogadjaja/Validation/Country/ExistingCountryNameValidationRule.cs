49e761a45bf28847f4dc7858ec55bf9bee75ec22
Darijan Mićić <darijan.micic10@gmail.com>
2023-06-07 18:16:47 +0200

    feat: create dialog for editing a country

---
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/Validation/Country/ExistingCountryNameValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ICR_2022_2023_Mapa_dogadjaja.Validation.Country
+{
+    public class ExistingCountryNameValidationRule : ValidationRule
+    {
+        public ExistingCountryNameValidationRule() { }
+        
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var enteredName = value as string;
+            if (string.IsNullOrWhiteSpace(enteredName))
+            {
+                return new ValidationResult(false, "Naziv države mora biti unet!");
+            }
+
+            string firstCharacterOfEnteredName = enteredName.Substring(0, 1);
+            if (firstCharacterOfEnteredName.Equals(firstCharacterOfEnteredName.ToLower()))
+            {
+                return new ValidationResult(false, "Naziv države mora započeti velikim slovom!");
+            }
+            
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ ICR_2022_2023_Mapa_dogadjaja/ICR_2022_2023_Mapa_dogadjaja/View/EditACountryDialog.xaml.cs
@@ -0,0 +1,156 @@
+﻿using ICR_2022_2023_Mapa_dogadjaja.Model;
+using ICR_2022_2023_Mapa_dogadjaja.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ICR_2022_2023_Mapa_dogadjaja.View
+{
+    /// <summary>
+    /// Interaction logic for EditACountryDialog.xaml
+    /// </summary>
+    public partial class EditACountryDialog : Window, INotifyPropertyChanged
+    {
+        private AllEntitiesViewModel allEntitiesViewModel;
+
+        private Country processedCountry;
+
+        private int validationErrorsCounter;
+
+        public EditACountryDialog(AllEntitiesViewModel allEntitiesViewModel, Country selectedCountry)
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            this.allEntitiesViewModel = allEntitiesViewModel;
+            processedCountry = selectedCountry;
+            validationErrorsCounter = 0;
+
+            AddHotKeys();
+
+            // REFERENCE: https://stackoverflow.com/a/808190
+            AddHandler(System.Windows.Controls.Validation.ErrorEvent, new RoutedEventHandler(OnErrorEvent));
+        }
+
+        public AllEntitiesViewModel AllEntitiesViewModel
+        {
+            get { return allEntitiesViewModel; }
+            set
+            {
+                if (value != allEntitiesViewModel)
+                {
+                    allEntitiesViewModel = value;
+                    OnPropertyChanged("AllEntitiesViewModel");
+                }
+            }
+        }
+
+        public Country ProcessedCountry
+        {
+            get { return processedCountry; }
+            set
+            {
+                if (value != processedCountry)
+                {
+                    processedCountry = value;
+                    OnPropertyChanged("ProcessedCountry");
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // REFERENCE: https://codesamplez.com/development/wpf-hotkeys-c-sharp
+        private void AddHotKeys()
+        {
+            try
+            {
+                RoutedCommand saveCountryCommand = new RoutedCommand();
+                saveCountryCommand.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(saveCountryCommand, SaveCountry));
+
+                RoutedCommand closeDialogCommand = new RoutedCommand();
+                closeDialogCommand.InputGestures.Add(new KeyGesture(Key.Q, ModifierKeys.Control));
+                CommandBindings.Add(new CommandBinding(closeDialogCommand, CloseDialog));
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.StackTrace);
+            }
+        }
+
+        // REFERENCE: https://stackoverflow.com/a/808190
+        private void OnErrorEvent(object sender, RoutedEventArgs e)
+        {
+            var validationEventArgs = e as ValidationErrorEventArgs;
+            if (validationEventArgs == null)
+            {
+                throw new Exception("Unexpected event args!");
+            }
+
+            switch (validationEventArgs.Action)
+            {
+                case ValidationErrorEventAction.Added:
+                    {
+                        validationErrorsCounter++;
+                        break;
+                    }
+                case ValidationErrorEventAction.Removed:
+                    {
+                        validationErrorsCounter--;
+                        break;
+                    }
+                default:
+                    {
+                        throw new Exception("Unknown action!");
+                    }
+            }
+
+            if (validationErrorsCounter == 0)
+            {
+                Save_button.IsEnabled = true;
+            }
+            else
+            {
+                Save_button.IsEnabled = false;
+            }
+        }
+
+        private void SaveCountry(object sender, RoutedEventArgs e)
+        {
+            if (Processed_country_form_grid == null)
+            {
+                return;
+            }
+
+            if (!Save_button.IsEnabled)
+            {
+                return;
+            }
+
+            allEntitiesViewModel.CountriesViewModel.Save();
+            
+            DialogResult = true;
+        }
+
+        // REFERENCE: https://learn.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-close-window-dialog-box?source=recommendations&view=netdesktop-7.0
+        private void CloseDialog(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
